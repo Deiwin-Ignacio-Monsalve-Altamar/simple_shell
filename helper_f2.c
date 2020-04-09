0e440c6f94bc359734df4cf7ea4812b696cb2bb8
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-04-09 20:46:08 +0000

    adding function _strcmp and updating file holberton.h

--- helper_f2.c
+++ helper_f2.c
@@ -20,3 +20,25 @@ size_t contokens(char *buff)
 	free(aux_buff);
 	return (count);
 }
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if they are equal or a number less
+ * than 0 if they are different
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	for (x = 0; s1[x] != '\0' && s2[x] != '\0'; x++)
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+	}
+	return (0);
+}
+
